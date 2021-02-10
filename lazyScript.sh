146ec751debb0a5e352bd83577c569bb48b3f34b
Altidor <altidorjb@gmail.com>
2021-02-10 17:07:28 -0500

    Lazy Push

--- lazyScript.sh
+++ lazyScript.sh
@@ -3,10 +3,12 @@ echo "Lazy Pull/Pusher"
 echo "Enter the update interval (in minutes) between each push (15 => 15 mins between updates)"
 read scale
 
-until ([[ "$scale" =~ ^[0-9]+$ ]]);
-{
-    
-}
+until [[ "$scale" =~ ^[0-9]+$ ]];
+do
+read scale
+echo "Sorry integers only"
+done
+echo "Is number!"
 if ! [[ "$scale" =~ ^[0-9]+$ ]]
     then
         echo "Sorry integers only"
