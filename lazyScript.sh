1dd0b1dd5d2f9aad203ebf94d45cc3a2a0f4e7ba
Altidor <altidorjb@gmail.com>
2021-02-10 17:03:14 -0500

    Lazy Push

--- lazyScript.sh
+++ lazyScript.sh
@@ -1,5 +1,14 @@
 echo "Lazy Pull/Pusher"
 echo "Enter the update interval (in minutes) between each push (15 => 15 mins between updates)"
+read scale
+do 
+{
+    
+}
+if ! [[ "$scale" =~ ^[0-9]+$ ]]
+    then
+        echo "Sorry integers only"
+fi
 
 
 n = 1
